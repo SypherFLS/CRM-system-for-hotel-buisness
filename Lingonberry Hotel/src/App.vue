19a243229c0c3914aeb922f16bcd51eaeeb6de75
Sypher <www.krasavinlv@gmail.com>
2025-05-19 00:26:39 +0300

    added header

--- Lingonberry Hotel/src/App.vue
+++ Lingonberry Hotel/src/App.vue
@@ -1,18 +1,53 @@
 <template>
-    <div class="app">
-        
+    <div id="app" class="body">
+        <div class="top-part">
+            <div class="toper-container">
+                <img class="background-pic" src="@/assets/hotel_pic.png" alt="hotel" />
+            </div>
+            
+            <Header></Header>
+        </div>
     </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'; 
     export default {
-        
+        components: {
+            Header
+        }
     }
 </script>
 
 <style scoped>
-    .app {
+    * {
         margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    
+    .top-part {
+        margin: 0;
+        width: 100%;
+        height: 100vh; 
+        overflow: hidden;
+        position: relative;
+    }
+    
+    
+
+    .background-pic {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 110%;
+        height: auto;
+        max-height: 500px;
+        object-fit: cover; 
+        z-index: -1; 
     }
 
+    .header {
+        z-index: 1; 
+    }
 </style>
---
+++ Lingonberry Hotel/src/components/Header.vue
@@ -0,0 +1,99 @@
+<template>
+  <header class="header">
+    <div class="logo-container">
+      <img src="@/assets/logo.png" alt="Logo" />
+    </div>
+    <nav class="nav-bar">
+      <ul class="nav-list">
+        <li><a href="#">Главная</a></li>
+        <li><a href="#">Наши отели</a></li>
+        <li><a href="#">Брусничные Баллы</a></li>
+        <li><a href="#">Контакты</a></li>
+        <li><a href="#">О нас</a></li>
+      </ul>
+    </nav>
+    <div class="account-container">
+        <img src="@/assets/login_pic.png" alt="account" />
+        <img src="@/assets/lines_pic.png" alt="account" />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+.header {
+  margin: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: transparent;
+  position: sticky;
+  top: 0;
+  z-index: 1000;
+}
+
+.logo-container img {
+  width: 100px;
+  height: auto;
+  padding-left: 20px;
+  object-fit: cover;
+}
+
+.nav-bar {
+  
+  flex-grow: 1;
+  text-align: center;
+}
+
+.nav-list {
+  font-size: 30px;
+  color: azure;
+  font-family: sans-serif;
+  display: inline-block;
+}
+
+.nav-bar ul {
+  list-style-type: none;
+  display: flex;
+  gap: 1.5rem;
+  justify-content: center; 
+}
+
+.account-container {
+  justify-self: right;
+}
+
+.account-container img {
+  filter: invert(100%);
+  width: 40px;
+  height: auto;
+  padding-right: 20px;
+}
+
+.nav-bar a {
+  text-decoration: none;
+  color: inherit;
+  transition: all .3s ease-in-out;
+}
+
+.nav-bar a:hover {
+  transform: scale(1.05);
+  color: #951c1c;
+}
+
+@media screen and (max-width: 768px) {
+  .nav-bar ul {
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+
+  .nav-bar li {
+    margin-bottom: 0.5rem;
+  }
+}
+</style>
