7bb13b5361b80bbe73d0dbe0b7942abd51f539aa
Sypher <www.krasavinlv@gmail.com>
2025-05-21 01:17:38 +0300

    заложен бэкэнд

--- Lingonberry Hotel/src/components/Registration-form.vue
+++ Lingonberry Hotel/src/components/Registration-form.vue
@@ -1,18 +1,19 @@
-<!-- RegistrationForm.vue -->
 <template>
   <div class="form-back">
     <form @submit.prevent="registerUser">
       <label for="name">Имя:</label>
-      <input type="text" v-model="user.name" required placeholder="Ваше имя"><br>
+      <input type="text" v-model.trim="user.name" required placeholder="Ваше имя" /><br />
       
       <label for="email">Email:</label>
-      <input type="email" v-model="user.email" required placeholder="example@example.com"><br>
+      <input type="email" v-model.trim="user.email" required placeholder="example@example.com" /><br />
       
       <label for="password">Пароль:</label>
-      <input type="password" v-model="user.password" required placeholder="Введите пароль"><br>
+      <input type="password" v-model="user.password" required placeholder="Введите пароль" /><br />
       
       <label for="confirm-password">Повторите Пароль:</label>
-      <input type="password" required placeholder="Введите пароль повторно"><br>
+      <input type="password" v-model="confirmPassword" required placeholder="Введите пароль повторно" /><br />
+
+      <p v-if="errorMessage" style="color: red;">{{ errorMessage }}</p>
       
       <button type="submit">Зарегистрироваться</button>
     </form>
@@ -20,6 +21,8 @@
 </template>
 
 <script>
+import axios from 'axios'; 
+
 export default {
   data() {
     return {
@@ -27,13 +30,43 @@ export default {
         name: '',
         email: '',
         password: ''
-      }
+      },
+      confirmPassword: '',
+      errorMessage: ''
     };
   },
   methods: {
-    registerUser() {
-      alert(`Пользователь ${this.user.name} зарегистрирован.`);
-      console.log(this.user);
+    async registerUser() {
+      this.errorMessage = '';
+
+      
+      if (this.user.password !== this.confirmPassword) {
+        this.errorMessage = 'Пароли не совпадают.';
+        return;
+      }
+
+      
+      if (this.user.password.length < 6) {
+        this.errorMessage = 'Пароль должен содержать минимум 6 символов.';
+        return;
+      }
+
+      try {
+        
+        const response = await axios.post('/api/register', this.user);
+
+        if (response.data.success) {
+          alert(`Пользователь ${this.user.name} успешно зарегистрирован.`);
+          this.user.name = '';
+          this.user.email = '';
+          this.user.password = '';
+          this.confirmPassword = '';
+        } else {
+          this.errorMessage = response.data.message || 'Ошибка регистрации.';
+        }
+      } catch (error) {
+        this.errorMessage = error.response?.data?.message || 'Ошибка соединения с сервером.';
+      }
     }
   }
 };
@@ -41,16 +74,15 @@ export default {
 
 <style scoped>
 .form-back {
-  
   background-color: brown;
-  border-radius: 15px; 
-  padding: 20px; 
+  border-radius: 15px;
+  padding: 20px;
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
-  max-width: 400px; 
-  margin: 0 auto; 
+  max-width: 400px;
+  margin: 0 auto;
 }
 
 form {
